911bf1c05a76838b6bf82e1a85582790593423b6
Luiz Akazawa <luizakazawa@outlook.com>
2026-02-16 08:08:38 +0100

    Created in explore_data.ipynb a population code for the postgres database / Created a connection between screens and implemented a timer to know the time spent in music details screen

--- recomSystemPy/main.py
+++ recomSystemPy/main.py
@@ -2,31 +2,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
-# A data model for what we expect from React Native
-class UserRequest(BaseModel):
-    username: str
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Python Backend!"}
-
-@app.post("/login")
-def login_user(user: UserRequest):
-    return {"status": "success", "user": user.username, "token": "fake-jwt-token"}
-
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
-
-# 1. THE STORAGE: A simple list to hold data in memory
-# (Note: If you restart the server, this list clears out)
 music_database = []
-
 class MusicItem(BaseModel):
     title: str
     artist: str
+    duration: float
 
 # 2. THE RECEIVER (POST): Allows React Native to add data
 @app.post("/music")
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, TextInput, SafeAreaView, Text, View, TouchableOpacity, Image } from 'react-native';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { FlatList, Text, View, TouchableOpacity, Image, Button, Alert, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer, useFocusEffect, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 import styles from './styles/globalStyles.ts'
 
+
+const Stack = createNativeStackNavigator();
 
 const DATA = [
   { id: '1', title: 'Music 1' },
@@ -20,7 +26,7 @@ const DATA = [
 ];
 
 
-
+//Refactor components!!!!
 const connectionPython = async (endpoint, method="GET", data=null) => {
     try{
         const BASE_URL = 'http://localhost:8000';
@@ -57,21 +63,52 @@ const addMusic = async (title, artist, duration) => {
   }
 };
 
-const renderItem = ({ item }) => (
-    <View style={styles.logoView}>
-      <TouchableOpacity onPress = {() => addMusic (item.title, item.id, 50)} style={styles.gridItem}>
-        <Image style ={styles.musicLogo} source={require('./images/closer_image.jpg')} resizeMode='cover'/>
-      </TouchableOpacity>
-      <Text style={styles.musicName}>{item.title}</Text>
-  </View>
-);
+//Screen with music details
+const DetailsScreen = ({ route, navigation }) => {
+    const { item } = route.params;
+    const startTime = useRef(null);
+
+    //execute when screen appears (when item.id changes)
+    useFocusEffect(
+      useCallback(() => {
+        startTime.current = Date.now();
+
+        return () => {
+          const endTime = Date.now();
+          if(startTime.current){
+            const duration = (endTime - startTime.current) / 1000;
+            console.log(duration)
+            addMusic(item.title, item.id, duration)
+          }    
+        };
+
+      }, [item.id])
+    );
+    //Need to change this style and put into the folder ./styles
+    return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}>{item.title}</Text>
+      <Text>ID: {item.id}</Text>
+      
+      <Image 
+        style={{ width: 200, height: 200, marginVertical: 20, borderRadius: 10 }} 
+        source={require('./images/closer_image.jpg')} 
+        resizeMode='cover'
+      />
+
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title="Add this Music" onPress={() => addMusic(item.title, item.id, 0)} />
+    </View>
+  );
+}
 
 
-const App = () => {
+//HomeScreen to see all the music options
+const HomeScreen = ({ navigation }) => {
     const [serverData, setServerData] = useState(null);
     useEffect(() => {
         const loadData = async () => {
-            const result = await connectionPython('');
+            const result = await connectionPython('music', 'GET');
             if(result){
                 setServerData(result);
                 }
@@ -79,44 +116,71 @@ const App = () => {
             loadData();
         }, []);
 
+    //renders all musics
+    const renderItem = ({ item }) => (
+        <View style={styles.logoView}>
+        <TouchableOpacity 
+            style={styles.gridItem}
+
+            onPress={() => navigation.navigate('Details', { item: item })}
+        >
+            <Image style={styles.musicLogo} source={require('./images/closer_image.jpg')} resizeMode='cover' />
+        </TouchableOpacity>
+        <Text style={styles.musicName}>{item.title}</Text>
+        </View>
+    );
+
+    return (
+    <View style={{ flex: 1, backgroundColor: '#eee' }}>
+      <View style={styles.header_main}>
+        <View style={styles.left_group}>
+          <View style={styles.header_photo}></View>
+          <Text style={styles.header_name}> USER NAME </Text>
+        </View>
+        <TouchableOpacity>
+          <Text>OPTIONS</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ padding: 10, backgroundColor: '#eee', margin: 10 }}>
+        <Text style={{ fontWeight: 'bold' }}>Server Response:</Text>
+        <Text>
+          {serverData ? JSON.stringify(serverData, null, 2) : "Loading..."}
+        </Text>
+      </View>
+
+      {/*need to think about using the name of the music instead of logo/image*/}
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={DATA}
+          numColumns={2}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          contentContainerStyle={styles.gridContainer}
+        />
+      </View>
+    </View>
+  );
+
+}
+
+const App = () => {
   return (
     <SafeAreaProvider>
-        <SafeAreaView style={{flex: 1}} >
-
-            <View style={styles.header_main}>
-                <View style={styles.left_group}>
-                    <View style={styles.header_photo}>
-                    </View>
-                    <Text style={styles.header_name}> USER NAME </Text>
-                </View>
-                <TouchableOpacity>
-                    <Text>OPTIONS</Text>
-                </TouchableOpacity>
-            </View>
-
-
-
-            <View style={{ padding: 10, backgroundColor: '#eee', margin: 10 }}>
-                <Text style={{ fontWeight: 'bold' }}>Server Response:</Text>
-                <Text>
-                    {serverData
-                        ? JSON.stringify(serverData, null, 2)
-                        : "Loading..."
-                    }
-                </Text>
-            </View>
-            <View style= {{flex: 1}}>
-              <FlatList
-                data={DATA}
-                numColumns={2}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}
-                contentContainerStyle = {styles.gridContainer}
-              />
-              </View>
-
-
-        </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name="Details" 
+            component={DetailsScreen} 
+            options={{ title: 'Music Details' }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
